1ec7482e80fd35b64f9b43b1b158a778816ed71c
Artem Sydorov <sydorovay@gmail.com>
2023-03-29 21:39:33 +0200

    hw 8-1 finish

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,36 @@
-// Add imports above this line
-import { galleryItems } from './gallery-items';
-// Change code below this line
+// Описаний в документації
+import SimpleLightbox from "simplelightbox";
+// Додатковий імпорт стилів
+import "simplelightbox/dist/simple-lightbox.min.css";
+import { galleryItems } from './gallery-items.js';
+// // Change code below this line
+// console.log(galleryItems);
 
-console.log(galleryItems);
+// // отримати посилання на список
+const galleryList = document.querySelector('.gallery');
+
+//callback. створити розмітку і повернути динамічний рядок
+function createGallery(items) {
+  return items.reduce((acc, { preview, original, description }) => {
+    return (
+      acc +
+      `<li class="gallery__item">
+        <a class="gallery__link" href="${original}">
+          <img class="gallery__image" src="${preview}" alt="${description}" />
+        </a>
+      </li>`
+    );
+  }, '');
+}
+
+// // перебрати galleryItems з допомогою callback ф-ції
+const galleryMarkup = createGallery(galleryItems);
+
+// //додати розмітку в html
+galleryList.insertAdjacentHTML('beforeend', galleryMarkup);
+
+//Підпис зображень с затримкою
+const lightboxGallery = new SimpleLightbox(".gallery__item a", {
+  captionsData: "alt",
+  captionDelay: 250,
+});
