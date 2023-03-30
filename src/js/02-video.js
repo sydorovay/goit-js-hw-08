f7a592355ce92a5fd44bc44b432bbf87b77ae9a1
Artem Sydorov <sydorovay@gmail.com>
2023-03-30 22:47:25 +0200

    create vimeoPlayer (task-2)

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,16 @@
+import Player from '@vimeo/player';
+
+// отримати посилання на iframe
+const iframe = document.querySelector('iframe');
+
+// // Ініціалізувати плеєр
+const player = new Player(iframe);
+
+player.on('play', function () {
+  console.log('played the video!');
+});
+
+player.getVideoTitle().then(function (title) {
+  console.log('title:', title);
+});
 
