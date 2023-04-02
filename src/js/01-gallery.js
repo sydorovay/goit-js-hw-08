876cd3dd54b731b305e1ed131708ab33113058c7
Artem Sydorov <sydorovay@gmail.com>
2023-04-02 13:46:05 +0200

    format with prettier

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,4 @@
-
-import SimpleLightbox from "simplelightbox";
+import SimpleLightbox from 'simplelightbox';
 import 'simplelightbox/dist/simple-lightbox.min.css';
 import { galleryItems } from './gallery-items.js';
 // // Change code below this line
