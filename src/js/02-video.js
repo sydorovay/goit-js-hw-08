58286ac833ad7e11fd7124dba14560c97c545cce
Artem Sydorov <sydorovay@gmail.com>
2023-04-02 12:35:37 +0200

    finished task 8-2

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,4 +1,8 @@
+// імпортувати бібліотеку vimeo/player
 import Player from '@vimeo/player';
+
+// імпортувати бібліотеку lodash.throttle
+import throttle from 'lodash.throttle';
 
 // отримати посилання на iframe
 const iframe = document.querySelector('iframe');
@@ -6,11 +10,17 @@ const iframe = document.querySelector('iframe');
 // // Ініціалізувати плеєр
 const player = new Player(iframe);
 
-player.on('play', function () {
-  console.log('played the video!');
-});
+// почати відстежувати подію timeupdate. Кожного разу, коли подія 'timeupdate' відбувається, код записує час відтворення у localStorage з ключем 'videoplayer-current-time'.
+player.on(
+  'timeupdate',
+  throttle(function (data) {
+    localStorage.setItem('videoplayer-current-time', data.seconds);
+  }, 1000)
+);
 
-player.getVideoTitle().then(function (title) {
-  console.log('title:', title);
-});
-
+// перевірти наявність значення в localStorage.
+const currentTime = localStorage.getItem('videoplayer-current-time');
+// Якщо значення є, то відтворення відео встановити на цей час за допомогою методу setCurrentTime().
+if (currentTime) {
+  player.setCurrentTime(currentTime);
+}
